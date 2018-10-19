0292f16416e7be9388b56b0d30219ab5175416d1
cv007 <curtvm@msn.com>
2018-10-18 23:33:25 -0500

    updated

--- Commander.cpp
+++ Commander.cpp
@@ -2,73 +2,62 @@
 #include "WifiCredentials.hpp"
 
 //=============================================================================
-// root commands
-//=============================================================================
-static void sys(WiFiClient&, String);
-static void wifi(WiFiClient&, String);
-static void net(WiFiClient&, String);
-
-//=============================================================================
-// root command list - name:function
-//=============================================================================
-typedef void(*cmdfunc_t)(WiFiClient&, String);
-typedef struct { const char* cmd; cmdfunc_t func; } cmd_t;
-static cmd_t root_commands[] = {
-        { "sys", &sys },
-        { "wifi", &wifi },
-        { "net", &net },
-        { NULL, NULL }
-};
-
-//=============================================================================
-// all sub commands
+// commands
 //=============================================================================
 //sys
 static void sys_boot(WiFiClient&, String);
 static void sys_reboot(WiFiClient&, String);
 static void sys_erase_all(WiFiClient&, String);
-static cmd_t sys_commands[] = {
-        { "boot", &sys_boot },
-        { "reboot", &sys_reboot },
-        { "erase all", &sys_erase_all },
-        { NULL, NULL }
-};
 //wifi
 static void wifi_list(WiFiClient&, String);
 static void wifi_add(WiFiClient&, String);
 static void wifi_erase(WiFiClient&, String);
-static cmd_t wifi_commands[] = {
-        { "list", &wifi_list },
-        { "add", &wifi_add },
-        { "erase", &wifi_erase },
-        { NULL, NULL }
-};
 //net
 static void net_hostname(WiFiClient&, String);
 static void net_APname(WiFiClient&, String);
 static void net_mac(WiFiClient&, String);
-static cmd_t net_commands[] = {
-        { "hostname", &net_hostname },
-        { "APname", &net_APname },
-        { "mac", &net_mac },
-        { NULL, NULL }
+
+//=============================================================================
+// command list - name:function
+//=============================================================================
+typedef void(*cmdfunc_t)(WiFiClient&, String);
+typedef struct { const char* cmd; cmdfunc_t func; } cmd_t;
+
+static cmd_t commands[] = {
+        { "sys",        NULL },             //root command
+        { "boot",       sys_boot },         //sub commands
+        { "reboot",     sys_reboot },
+        { "erase all",  sys_erase_all },
+
+        { "wifi",       NULL },
+        { "list",       wifi_list },
+        { "add",        wifi_add },
+        { "erase",      wifi_erase },
+
+        { "net",        NULL },
+        { "hostname",   net_hostname },
+        { "APname",     net_APname },
+        { "mac",        net_mac },
+
+        { NULL,         NULL }              //end of table
 };
 
 //=============================================================================
 // common print functions
 //=============================================================================
-void help(WiFiClient& client, cmd_t* cmd)
+void help(WiFiClient& client)
 {
-    client.printf("available sub-commands:\n");
-    for(auto i = 0; cmd[i].cmd != NULL; i++){
-        client.printf("  %s\n", cmd[i].cmd);
+    client.printf("available commands:\n");
+    for(auto i = 0, ii = 0; commands[i].cmd != NULL; i++){
+        if(commands[i].func == NULL){
+            ii = i; //save root command index
+            continue; //don't print root command
+        }
+        client.printf("  %s %s\n", commands[ii].cmd, commands[i].cmd);
     }
-    client.printf("\n");
+    client.printf("\n$ ");
 }
-void bad(WiFiClient& client)
-{
-    client.printf("unknown command\n\n");
-}
+void bad(WiFiClient& client){ client.printf("unknown command\n\n$ "); }
 
 //=============================================================================
 // process incoming buffer passed from telnet function (will be 0 terminated)
@@ -85,108 +74,78 @@ bool Commander::process(WiFiClient& client, uint8_t* buf)
     s.trim(); //removed leading/trailing space
 
     //check for root command
-    for(auto c : root_commands){
-        if(not s.startsWith(c.cmd)) continue;
-        s.replace(c.cmd, ""); //remove command string
-        c.func(client, s);
-        client.printf("$ ");
+    for(auto i = 0; commands[i].func || commands[i].cmd; i++){
+        if(commands[i].func) continue; //only looking for root command
+        if(not s.startsWith(commands[i].cmd)) continue; //no match
+        //root command found
+        s.replace(commands[i].cmd, ""); //remove command string
+        if(s[0] != ' '){ help(client); return true;  } //no space after command
+        s.trim();
+        if(not s[0]){ help(client); return true; } //unfinshed command
+
+        //check for sub command
+        for(i++; commands[i].func; i++){
+            if(not s.startsWith(commands[i].cmd)) continue;
+            //sub command found
+            s.replace(commands[i].cmd, "");
+            s.trim();
+            //run command
+            commands[i].func(client, s);
+            client.printf("$ ");
+            return true;
+        }
+        //bad subcommand
+        help(client);
         return true;
     }
 
-    client.printf("unknown command\n\n$ ");
+    bad(client);
     return true; //clear buffer and start over
 }
-
-
-
 
 //=============================================================================
 // all command functions
 //=============================================================================
-//root commands
-void sys(WiFiClient& client, String s)
-{
-    if(not s[0]){ help(client, sys_commands); return; }
-    if(s[0] != ' '){ bad(client); return;  }
-    s.trim();
-    for(auto c : sys_commands){
-        if(not s.startsWith(c.cmd)) continue;
-        s.replace(c.cmd, "");
-        c.func(client, s);
-        return;
-    }
-    bad(client);
-}
-void wifi(WiFiClient& client, String s)
-{
-    if(not s[0]){ help(client, wifi_commands); return; }
-    if(s[0] != ' '){ bad(client); return;  }
-    s.trim();
-    for(auto c : wifi_commands){
-        if(not s.startsWith(c.cmd)) continue;
-        s.replace(c.cmd, "");
-        c.func(client, s);
-        return;
-    }
-    bad(client);
-}
-void net(WiFiClient& client, String s)
-{
-    if(not s[0]){ help(client, net_commands); return; }
-    if(s[0] != ' '){ bad(client); return;  }
-    s.trim();
-    for(auto c : net_commands){
-        if(not s.startsWith(c.cmd)) continue;
-        s.replace(c.cmd, "");
-        c.func(client, s);
-        return;
-    }
-    bad(client);
-}
-
-//sys
+//sys boot
 static void sys_boot(WiFiClient& client, String s)
 {
-    //"sys boot"
+    //no args
     if(not s[0]){
         WifiCredentials wifidata;
         client.printf("boot: %s\n", wifidata.boot() ? "AP" : "STA");
-        return;
     }
-    //sys boot=AP
-    if(s.startsWith("AP")){
+    //=AP
+    else if(s.startsWith("=AP")){
         WifiCredentials wifidata;
         wifidata.boot(wifidata.AP);
     }
-    //sys boot=STA
-    else if(s.startsWith("STA")){
+    //=STA
+    else if(s.startsWith("=STA")){
         WifiCredentials wifidata;
         wifidata.boot(wifidata.STA);
-        return;
     }
-    else bad(client);
+    else help(client);
 }
+//sys reboot
 static void sys_reboot(WiFiClient& client, String s)
 {
-    //"sys reboot"
     if(s[0]){ bad(client); return; }
     client.printf("rebooting in 5 seconds...");
     delay(5000);
     ESP.restart();
 }
+//sys erase all
 static void sys_erase_all(WiFiClient& client, String s)
 {
-    //"sys erase all"
     if(s[0]){ bad(client); return; }
     client.printf("erasing all stored data...");
     WifiCredentials wifidata;
     wifidata.erase_all();
     client.printf("done.\n");
 }
-//wifi
+//wifi list
 static void wifi_list(WiFiClient& client, String s)
 {
-    //"wifi list"
     if(s[0]){ bad(client); return; }
     WifiCredentials wifidata;
     int max = wifidata.maxn();
@@ -197,11 +156,11 @@ static void wifi_list(WiFiClient& client, String s)
         );
     }
 }
+//wifi add
 static void wifi_add(WiFiClient& client, String s)
 {
-    //"wifi add"
-    if(s[0] != ' '){ bad(client); return;  }
-    s = s.substring(1);
+    if(s[0] != ' '){ help(client); return;  }
+    s.trim();
     WifiCredentials wifidata;
     //"wifi add 0 ssid=myssid"
     //"wifi add 0 pass=mypass"
@@ -229,16 +188,16 @@ static void wifi_add(WiFiClient& client, String s)
         wifidata.pass(idx, &s[si+5]);
         return;
     }
-    bad(client);
+    help(client);
 }
+//wifi erase
 static void wifi_erase(WiFiClient& client, String s)
 {
-    //"wifi erase"
-    if(s[0] != ' '){ bad(client); return; }
-    s = s.substring(1);
+    if(s[0] != ' '){ help(client); return; }
+    s.trim();
     //"wifi erase 0"
     int idx = 0;
-    if(s.substring(0,1) != "0"){
+    if(s != "0"){
         idx = s.toInt();
         if(idx == 0){
             client.printf("missing index# or index# not valid\n");
@@ -253,19 +212,21 @@ static void wifi_erase(WiFiClient& client, String s)
     wifidata.ssid(idx, "");
     wifidata.pass(idx, "");
 }
-//net
+//net hostname
 static void net_hostname(WiFiClient& client, String s)
 {
-    //"net hostname"
+    //no arg
     if(not s[0]){
         WifiCredentials wifidata;
-        client.printf("%s\n", WiFi.getHostname());
+        client.printf("hardware: %s  stored: %s\n",
+            WiFi.getHostname(), wifidata.hostname().c_str()
+        );
         return;
     }
-    //"net hostname=myname"
+    //=myname
     if(s[0] == '='){
         WifiCredentials wifidata;
-        s = s.substring(1);
+        s.trim();
         if(s.length() > 32){
             client.printf("hostname too long (32 chars max)\n");
             return;
@@ -274,20 +235,21 @@ static void net_hostname(WiFiClient& client, String s)
         WiFi.setHostname(s.c_str()); //and out to tcpip (may not see until reboot)
         return;
     }
-    bad(client); return;
+    help(client);
 }
+//net APname
 static void net_APname(WiFiClient& client, String s)
 {
-    //"net APname"
+    //no arg
     if(not s[0]){
         WifiCredentials wifidata;
         client.printf("%s\n", wifidata.APname().c_str());
         return;
     }
-    //"net APname=myAPname"
+    //=myAPname
     if(s[0] == '='){
         WifiCredentials wifidata;
-        s = s.substring(1);
+        s.trim();
         if(s.length() > 32){
             client.printf("APname too long (32 chars max)\n");
             return;
@@ -295,12 +257,12 @@ static void net_APname(WiFiClient& client, String s)
         wifidata.APname(s); //nvs storage
         return;
     }
-    bad(client); return;
+    help(client);
 }
+//net mac
 static void net_mac(WiFiClient& client, String s)
 {
-    //"net mac"
-    if(s[0]){ bad(client); return;  }
+    if(s[0]){ help(client); return;  }
     client.printf("%s\n", WiFi.macAddress().c_str());
 }
 
